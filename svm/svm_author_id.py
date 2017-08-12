7d17f3bdb1b732ab3d2cf77fee33ea4566338f5a
tutysara <tutysara@gmail.com>
2017-08-12 22:15:24 +0000

    completed svm chapter

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -27,10 +26,19 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 from sklearn import svm
-clf = svm.SVC(kernel="linear", gamma=1)
-clf.fit(features_train, labels_train)
-pred = clf.predict(features_test)
-
-from sklearn.metrics import accuracy_score
-accuracy = accuracy_score(labels_test, pred)
-print accuracy
+c_values = [10000]
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+for c in c_values:
+    clf = svm.SVC(kernel="rbf", C=c)
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    pred = clf.predict(features_test)
+    t1 = time()
+    print "time taken to train : " + str(t1-t0) + " sec"
+    from sklearn.metrics import accuracy_score
+    accuracy = accuracy_score(labels_test, pred)
+    print "c:", c, "accuracy", accuracy
+    pred_indexs = [10, 26, 50]
+    for pred_index in pred_indexs:
+        print "prediction for", pred_index, pred[pred_index]
